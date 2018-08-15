531cae4325701cc75021e71ef0fed0ae63a35a06
Nimesh Ghelani <nimeshghelani@gmail.com>
2018-08-15 08:06:39 -0400

    Final v1

--- gen_tables.py
+++ gen_tables.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 datasets = ['athome1', 'athome2', 'athome3', 'athome4']
 strategies = ['exponential', 'static', 'partial', 'precision', 'recency']
@@ -40,7 +41,10 @@ for dataset in datasets:
         data[dataset][strategy] = {}
         strategy_path = os.path.join(results_path, strategy)
         for parameter in parameters[strategy]:
-            data[dataset][strategy][parameter] = {}
+            label = strategy
+            if parameter:
+                label += '(' + parameter + ')'
+            data[dataset][strategy][parameter] = {'label': label}
             metrics = data[dataset][strategy][parameter]
             # Gather metrics
             with open(os.path.join(strategy_path, parameter, 'log')) as logfile:
@@ -51,7 +55,7 @@ for dataset in datasets:
                         val = val.strip()
                         if key in METRICS:
                             metrics[key] = float(val)
-            assert(len(metrics) == 8)
+            assert(len(metrics) == 9)
             # Gather gain curve statistics
             topic_gain_curves = {}
             topic_eff_75 = {}
@@ -81,15 +85,12 @@ for dataset in datasets:
             while R < 2.01:
                 gain_curve_x.append(R)
                 y_val = 0
-                try:
-                    for topic in topic_gain_curves:
-                        y_val += topic_gain_curves[topic][int(R * topics[dataset][topic]['rels'])]
-                except:
-                    break
+                for topic in topic_gain_curves:
+                    y_val += topic_gain_curves[topic][int(max(R-0.01, 0) * topics[dataset][topic]['rels'])]
                 y_val /= len(topic_gain_curves)
                 gain_curve_y.append(y_val)
                 R += 0.1
-            metrics['gain_curve'] = (gain_curve_x, gain_curve_y)
+            metrics['gain_curve'] = (np.array(gain_curve_x), np.array(gain_curve_y))
             metrics['0.75 Effort'] = sum(topic_eff_75.values()) / len(topic_eff_75)
 
 
@@ -101,14 +102,19 @@ for strategy in strategies:
         for metric in METRICS:
             average[strategy][parameter][metric] = 0
             for dataset in datasets:
+                average[strategy][parameter]['label'] = data[dataset][strategy][parameter]['label']
                 average[strategy][parameter][metric] += data[dataset][strategy][parameter][metric]
             average[strategy][parameter][metric] /= len(datasets)
+        average[strategy][parameter]['label'] = data[dataset][strategy][parameter]['label']
+        average[strategy][parameter]['gain_curve'] = sum(data[dataset][strategy][parameter]['gain_curve'][1] for dataset in datasets) / len(datasets)
+        average[strategy][parameter]['gain_curve'] = (data[datasets[0]][strategy][parameter]['gain_curve'][0], average[strategy][parameter]['gain_curve'])
 
 
 def to_table_row(data, strategy, parameter):
     label = strategy
     if parameter:
         label += '(' + parameter + ')'
+    label = '\\texttt{%s}' % label
     vals = []
     for metric in ['Recall at 1', 'Recall at 1.5', 'Recall at 2', '0.75 Effort', ]:
         vals.append('%.3f' % data[strategy][parameter][metric])
@@ -125,4 +131,82 @@ def to_table(dataset):
         lines[-1] = lines[-1] + ' \\hline'
     return '\n'.join(lines)
 
-def plot(curves)
+import matplotlib.pyplot as plt
+markers = ['s', 'o', '^', 'x']
+markersizes = [7,5,5, 5]
+
+for idx, d in enumerate([average['exponential'][''], average['static']['k=1'], average['static']['k=100']]):
+    x_vals, y_vals = d['gain_curve']
+    plt.plot(x_vals, y_vals, label=d['label'], linewidth=1.0, marker=markers[idx], markersize=markersizes[idx])
+
+plt.xlabel('Effort')
+plt.ylabel('Avg. Recall')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.legend(loc='lower right', shadow=True)
+plt.savefig('./plots/bmi_static.pdf', bbox_inches="tight")
+plt.close()
+
+
+for idx, d in enumerate([average['static']['k=1'], average['partial']['k=10,s=1000'], average['partial']['k=500,s=1000']]):
+    x_vals, y_vals = d['gain_curve']
+    plt.plot(x_vals, y_vals, label=d['label'], linewidth=1.0, marker=markers[idx], markersize=markersizes[idx])
+
+plt.xlabel('Effort')
+plt.ylabel('Avg. Recall')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.legend(loc='lower right', shadow=True)
+plt.savefig('./plots/static_partial.pdf', bbox_inches="tight")
+plt.close()
+
+for idx, d in enumerate([average['partial']['k=100,s=1000'], average['partial']['k=100,s=5000'], average['partial']['k=100,s=100']]):
+    x_vals, y_vals = d['gain_curve']
+    plt.plot(x_vals, y_vals, label=d['label'], linewidth=1.0, marker=markers[idx], markersize=markersizes[idx])
+
+plt.xlabel('Effort')
+plt.ylabel('Avg. Recall')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.legend(loc='lower right', shadow=True)
+plt.savefig('./plots/partial2.pdf', bbox_inches="tight")
+plt.close()
+
+
+for idx, d in enumerate([average['precision']['p=1.0,m=25'], average['precision']['p=0.8,m=25'], average['precision']['p=0.4,m=25']]):
+    x_vals, y_vals = d['gain_curve']
+    plt.plot(x_vals, y_vals, label=d['label'], linewidth=1.0, marker=markers[idx], markersize=markersizes[idx])
+
+plt.xlabel('Effort')
+plt.ylabel('Avg. Recall')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.legend(loc='lower right', shadow=True)
+plt.savefig('./plots/precision.pdf', bbox_inches="tight")
+plt.close()
+
+
+for idx, d in enumerate([average['static']['k=1,it=100000'], average['static']['k=1,it=10000'], average['static']['k=1,it=1000'], average['static']['k=1,it=100']]):
+    x_vals, y_vals = d['gain_curve']
+    plt.plot(x_vals, y_vals, label=d['label'], linewidth=1.0, marker=markers[idx], markersize=markersizes[idx])
+
+plt.xlabel('Effort')
+plt.ylabel('Avg. Recall')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.legend(loc='lower right', shadow=True)
+plt.savefig('./plots/training.pdf', bbox_inches="tight")
+plt.close()
+
+
+for idx, d in enumerate([average['static']['k=1,it=1000'], average['static']['k=1,it=100000'], average['recency']['w=25,it=1000'] ]):
+    x_vals, y_vals = d['gain_curve']
+    plt.plot(x_vals, y_vals, label=d['label'], linewidth=1.0, marker=markers[idx], markersize=markersizes[idx])
+
+plt.xlabel('Effort')
+plt.ylabel('Avg. Recall')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.legend(loc='lower right', shadow=True)
+plt.savefig('./plots/recency.pdf', bbox_inches="tight")
+plt.close()
